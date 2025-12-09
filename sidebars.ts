18dd3fe2833122f28cc82032c9fd5c40b43dd8af
haritha1313 <haritha1313@gmail.com>
2025-12-09 05:19:24 -0800

    sidebar fix

--- sidebars.ts
+++ sidebars.ts
@@ -3,14 +3,7 @@ import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
 const sidebars: SidebarsConfig = {
   docsSidebar: [
     'index',
-    {
-      type: 'category',
-      label: 'Getting Started',
-      items: [
-        'getting-started/installation',
-        'getting-started/quickstart',
-      ],
-    },
+    'getting-started',
     {
       type: 'category',
       label: 'Concepts',
